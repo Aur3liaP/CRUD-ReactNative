964028078f414d0bae8d0d86b6e605e3e6ef07e8
Pic Aurélia <pic.aurelia@gmail.com>
2024-10-13 21:22:02 +0200

    Add animation & sound when catching pokemon

--- clients/components/pokemon/PokemonCatch.tsx
+++ clients/components/pokemon/PokemonCatch.tsx
@@ -1,7 +1,8 @@
-import { Alert, Pressable, StyleSheet, View, type ViewProps, type ViewStyle } from "react-native";
+import { Animated, Alert, Pressable, StyleSheet, View, Image, Easing, type ViewProps} from "react-native";
+import { Audio } from "expo-av"; 
 import { ThemedText } from "../ThemedText";
 import axios from "axios";
-import { useCallback } from "react";
+import {  useState, useCallback, useRef } from "react";
 
 type Props = ViewProps & {
     pokemonData : {
@@ -23,6 +24,15 @@ type Props = ViewProps & {
 
 
 export function PokemonCatch({style, pokemonData, ...rest}: Props) {
+    const [isCatching, setIsCatching] = useState(false); // Pour déclencher l'animation
+    const pokeballPosition = useRef(new Animated.Value(0)).current; // Position de l'animation
+
+    const playSound = async () => {
+        const { sound } = await Audio.Sound.createAsync(
+            require("@/assets/PokeballSound.mp3"), {shouldPlay: true}
+        );
+        sound.playAsync();
+    };
     
     const handleCatch = useCallback(async () => {
         try {
@@ -47,12 +57,65 @@ export function PokemonCatch({style, pokemonData, ...rest}: Props) {
         }
       }, [pokemonData]);
 
+      const startPokeballAnimation = () => {
+        playSound(); 
+        setIsCatching(true);
+        Animated.timing(pokeballPosition, {
+            toValue: 1,
+            duration: 1000,
+            useNativeDriver: true,
+            easing: Easing.bezier(0.22, 1, 0.36, 1),
+        }).start(() => {
+            setIsCatching(false);
+            handleCatch(); 
+        });
+    };
+
+    const pokeballStyle = {
+        transform: [
+            {
+                translateY: pokeballPosition.interpolate({
+                    inputRange: [0, 1],
+                    outputRange: [0, -300], 
+                }),
+            },
+                    {
+            translateX: pokeballPosition.interpolate({
+                inputRange: [0, 0.5, 1],
+                outputRange: [0, 50, 0], 
+            }),
+        },
+            {
+                scale: pokeballPosition.interpolate({
+                    inputRange: [0, 1],
+                    outputRange: [1, 0.2], 
+                }),
+            },
+            {
+                rotate: pokeballPosition.interpolate({
+                    inputRange: [0, 1],
+                    outputRange: ["0deg", "360deg"],
+                }),
+            },
+        ],
+    };
+
 
     return (
         <View style={styles.release}>
-            <Pressable onPress={handleCatch}>
+            <Pressable onPress={startPokeballAnimation}>
                 <ThemedText color="grayWhite" variant="subtitle">Capturer le Pokemon !</ThemedText>
             </Pressable>
+
+            {isCatching && (
+                <Animated.View style={[styles.pokeball, pokeballStyle]}>
+                    <Image
+                        source={require("@/assets/images/PokeRocket.webp")} 
+                        style={{ width: 150, height: 150 }}
+                    />
+                </Animated.View>
+            )}
+
         </View>
     )
 }
@@ -61,5 +124,11 @@ const styles = StyleSheet.create({
     release: {
         alignItems:"center",
         paddingTop: 108,
+    },
+    pokeball: {
+        position: "absolute", 
+        bottom: 100, 
+        left: "50%",
+        marginLeft: -50,
     },
 });
